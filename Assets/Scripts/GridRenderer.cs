44a2ed6caaa319f496626ab4e0221e7278071057
Ayoub Gharbi <ayoub@truluv.ai>
2022-10-07 09:51:57 -0400

    Workshop #3.

--- Assets/Scripts/GridRenderer.cs
+++ Assets/Scripts/GridRenderer.cs
@@ -1,20 +1,40 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridRenderer : MonoBehaviour
 {
-    [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
+    [Range(1, 10)] [SerializeField] private int gridWidth;
+    [Range(1, 10)] [SerializeField] private int gridHeight;
     [SerializeField] private GameObject cellBlockPrefab;
+
+    private BlockState[,] _grid;
+    private List<Vector3> _gridCellPositions = new List<Vector3>();
+    private List<GameObject> _gridCellInstances = new List<GameObject>();
 
     private void Start()
     {
         RenderGrid();
     }
 
+    private void OnValidate()
+    {
+        if (!Application.isPlaying) return;
+
+        RenderGrid();
+    }
+
+    private void CleanUp()
+    {
+        _gridCellInstances.ForEach(Destroy);
+        _gridCellInstances = new List<GameObject>();
+        _gridCellPositions = new List<Vector3>();
+    }
+
     private void RenderGrid()
     {
-        BlockState[,] grid = GridGenerator.GenerateGrid(gridWidth, gridHeight);
+        CleanUp();
+
+        _grid = GridGenerator.GenerateGrid(gridWidth, gridHeight);
 
         float cellSize = cellBlockPrefab.transform.localScale.z;
         float halfCellSize = cellSize * 0.5f;
@@ -23,43 +43,56 @@ public class GridRenderer : MonoBehaviour
         {
             for (int z = 0; z < gridHeight; z++)
             {
-                BlockState gridCellState = grid[x, z];
+                BlockState gridCellState = _grid[x, z];
                 Vector3 gridCellCenterPos = new Vector3(x * cellSize, 0, z * cellSize);
+
+                _gridCellPositions.Add(gridCellCenterPos);
 
                 if (gridCellState.HasFlag(BlockState.Left))
                 {
-                    GameObject cellBlock = Instantiate(cellBlockPrefab, this.transform);
                     Vector3 cellPos = gridCellCenterPos + new Vector3(-halfCellSize, 0, 0);
-                    cellBlock.transform.position = cellPos;
+                    CreateBlock(cellPos, Vector3.zero);
                 }
 
                 if (gridCellState.HasFlag(BlockState.Up))
                 {
-                    GameObject cellBlock = Instantiate(cellBlockPrefab, this.transform);
                     Vector3 rotationAngle = new Vector3(0, 90, 0);
-                    cellBlock.transform.eulerAngles = rotationAngle;
-                    Vector3 cellPos =  gridCellCenterPos + new Vector3(0, 0, halfCellSize);
-                    cellBlock.transform.position = cellPos;
+                    Vector3 cellPos = gridCellCenterPos + new Vector3(0, 0, halfCellSize);
+                    CreateBlock(cellPos, rotationAngle);
                 }
 
                 if (gridCellState.HasFlag(BlockState.Right))
                 {
-                    GameObject cellBlock = Instantiate(cellBlockPrefab, this.transform);
                     Vector3 rotationAngle = new Vector3(0, 180, 0);
-                    cellBlock.transform.eulerAngles = rotationAngle;
-                    Vector3 cellPos =  gridCellCenterPos + new Vector3(halfCellSize, 0,0 );
-                    cellBlock.transform.position = cellPos;
+                    Vector3 cellPos = gridCellCenterPos + new Vector3(halfCellSize, 0, 0);
+                    CreateBlock(cellPos, rotationAngle);
                 }
 
                 if (gridCellState.HasFlag(BlockState.Down))
                 {
-                    GameObject cellBlock = Instantiate(cellBlockPrefab, this.transform);
                     Vector3 rotationAngle = new Vector3(0, -90, 0);
-                    cellBlock.transform.eulerAngles = rotationAngle;
-                    Vector3 cellPos =  gridCellCenterPos + new Vector3(0, 0,-halfCellSize);
-                    cellBlock.transform.position = cellPos;
+                    Vector3 cellPos = gridCellCenterPos + new Vector3(0, 0, -halfCellSize);
+                    CreateBlock(cellPos, rotationAngle);
                 }
             }
         }
+    }
+
+    private void CreateBlock(Vector3 position, Vector3 angle)
+    {
+        GameObject cellBlock = Instantiate(cellBlockPrefab, this.transform);
+        cellBlock.transform.eulerAngles = angle;
+        cellBlock.transform.position = position;
+
+        _gridCellInstances.Add(cellBlock);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_gridCellPositions is not { Count: > 0 }) return;
+
+        Gizmos.color = Color.magenta;
+        foreach (Vector3 gridCellPos in _gridCellPositions)
+            Gizmos.DrawWireSphere(gridCellPos, 0.25f);
     }
 }
